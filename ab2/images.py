e8bb9b04f734181f0d938a59de75248925ac2d0c
Daniel Bartsch <bartschdf@gmail.com>
2024-03-15 14:06:53 +0100

    evc: AB2 - 4.1

--- ab2/images.py
+++ ab2/images.py
@@ -4,7 +4,6 @@ from PIL import Image
 
 import utils
 
-
 def read_img(inp:str) -> Image.Image:
     """
         Returns a PIL Image given by its input path.
@@ -17,12 +16,8 @@ def convert(img:Image.Image) -> np.ndarray:
         Converts a PIL image [0,255] to a numpy array [0,1].
     """
     ### STUDENT CODE
-    # TODO: Implement this function.
-
-	# NOTE: The following lines can be removed. They prevent the framework
-    #       from crashing.
-
-    out = np.zeros((512,512,3))
+    
+    out = np.array(img) / 255.0
 
     ### END STUDENT CODE
     return out
@@ -32,12 +27,8 @@ def switch_channels(img:np.ndarray) -> np.ndarray:
         Swaps the red and green channel of a RGB image given by a numpy array.
     """
     ### STUDENT CODE
-    # TODO: Implement this function.
-
-	# NOTE: The following lines can be removed. They prevent the framework
-    #       from crashing.
-
-    out = np.zeros(img.shape)
+    
+    out = img[:, :, [1, 0, 2]]
 
     ### END STUDENT CODE
 
@@ -48,12 +39,10 @@ def image_mark_green(img:np.ndarray) -> np.ndarray:
         returns a numpy-array (HxW) with 1 where the green channel of the input image is greater or equal than 0.7, otherwise zero.
     """
     ### STUDENT CODE
-    # TODO: Implement this function.
-
-	# NOTE: The following lines can be removed. They prevent the framework
-    #       from crashing.
-
-    mask = np.zeros(img.shape)
+    
+    mask = img[:, :, 1] >= 0.7
+    
+    mask = mask.astype(int)
 
     ### END STUDENT CODE
 
@@ -65,12 +54,9 @@ def image_masked(img:np.ndarray, mask:np.ndarray) -> np.ndarray:
         sets the pixels of the input image to zero where the mask is 1.
     """
     ### STUDENT CODE
-    # TODO: Implement this function.
-
-	# NOTE: The following lines can be removed. They prevent the framework
-    #       from crashing.
-
-    out = np.zeros(img.shape)
+    
+    out = img.copy()
+    out[mask == 1] = 0
 
     ### END STUDENT CODE
 
@@ -81,13 +67,9 @@ def grayscale(img:np.ndarray) -> np.ndarray:
         Returns a grayscale image of the input. Use utils.rgb2gray().
     """
     ### STUDENT CODE
-    # TODO: Implement this function.
 
-	# NOTE: The following lines can be removed. They prevent the framework
-    #       from crashing.
-
-    out = np.zeros(img.shape)
-
+    out = utils.rgb2gray(img)
+    
     ### END STUDENT CODE
 
     return out
@@ -97,12 +79,13 @@ def cut_and_reshape(img_gray:np.ndarray) -> np.ndarray:
         Cuts the image in half (x-dim) and stacks it together in y-dim.
     """
     ### STUDENT CODE
-    # TODO: Implement this function.
-
-	# NOTE: The following lines can be removed. They prevent the framework
-    #       from crashing.
-
-    out = np.zeros(img_gray.shape)
+   
+    midpoint = img_gray.shape[1] // 2
+    
+    left_half = img_gray[:, :midpoint]
+    right_half = img_gray[:, midpoint:]
+    
+    out = np.vstack((left_half, right_half))
 
     ### END STUDENT CODE
 
