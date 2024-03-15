15af0c10cc293e9ff097c30e3f5d766dec47a577
Daniel Bartsch <bartschdf@gmail.com>
2024-03-15 12:43:00 +0100

    evc: AB2 - 3

--- ab2/triangles.py
+++ ab2/triangles.py
@@ -2,80 +2,126 @@ from typing import List, Tuple
 
 import numpy as np
 
+
+matrikelnummer = "12323792"
+
+d = {
+    'A': int(matrikelnummer[0]),
+    'B': int(matrikelnummer[1]),
+    'C': int(matrikelnummer[2]),
+    'D': int(matrikelnummer[3]),
+    'E': int(matrikelnummer[4]),
+    'F': int(matrikelnummer[5]),
+    'G': int(matrikelnummer[6]),
+    'H': int(matrikelnummer[7]),
+}
+
+
 def define_triangle() -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     ### STUDENT CODE
-    # TODO: Implement this function.
 
-	# NOTE: The following lines can be removed. They prevent the framework
-    #       from crashing.
+    P1 = np.array((1+d['C'], -(1+d['A']), -(1+d['E'])))
+    P2 = np.array((-(1+d['G']), -(1+d['B']), 1+d['H']))
+    P3 = np.array((-(1+d['D']), 1+d['F'], -(1+d['B'])))
 
-    P1 = np.zeros(3)
-    P2 = np.zeros(3)
-    P3 = np.zeros(3)
     ### END STUDENT CODE
 
     return P1, P2, P3
 
+P1, P2, P3 = define_triangle()
+#print("P1:", P1)
+#print("P2:", P2)
+#print("P3:", P3)
+
 def define_triangle_vertices(P1:np.ndarray, P2:np.ndarray, P3:np.ndarray) -> Tuple[np.ndarray, np.ndarray, np.ndarray]:
     ### STUDENT CODE
-    # TODO: Implement this function.
+    
+    P1P2 = P2 - P1
+    P2P3 = P3 - P2
+    P3P1 = P1 - P3
 
-	# NOTE: The following lines can be removed. They prevent the framework
-    #       from crashing.
-
-    P1P2 = np.zeros(3)
-    P2P3 = np.zeros(3)
-    P3P1 = np.zeros(3)
     ### END STUDENT CODE
 
     return P1P2, P2P3, P3P1
 
+P1P2, P2P3, P3P1 = define_triangle_vertices(P1, P2, P3)
+#print("P1P2:", P1P2)
+#print("P2P3:", P2P3)
+#print("P3P1:", P3P1)
+
 def compute_lengths(P1P2:np.ndarray, P2P3:np.ndarray, P3P1:np.ndarray) -> List[float]:
     ### STUDENT CODE
-    # TODO: Implement this function.
+    
+    x = np.sqrt((P1P2[0]**2 + P1P2[1]**2 + P1P2[2]**2))
+    y = np.sqrt((P2P3[0]**2 + P2P3[1]**2 + P2P3[2]**2))
+    z = np.sqrt((P3P1[0]**2 + P3P1[1]**2 + P3P1[2]**2))
 
-	# NOTE: The following lines can be removed. They prevent the framework
-    #       from crashing.
+    norms = [x, y, z]
 
-    norms = [0., 0., 0.]
     ### END STUDENT CODE
 
     return norms
 
+l = compute_lengths(P1P2, P2P3, P3P1)
+#print("my Length:", l)
+#print("np Length:", [np.linalg.norm(P1P2), np.linalg.norm(P2P3), np.linalg.norm(P3P1)])
+
 def compute_normal_vector(P1P2:np.ndarray, P2P3:np.ndarray, P3P1:np.ndarray) -> Tuple[np.ndarray, np.ndarray]:
     ### STUDENT CODE
-    # TODO: Implement this function.
+    
+    n = np.cross(P2P3 - P1P2, P3P1 - P1P2)
+    n_normalized = n / np.linalg.norm(n)
 
-	# NOTE: The following lines can be removed. They prevent the framework
-    #       from crashing.
-
-    n = np.zeros(3)
-    n_normalized = np.zeros(3)
     ### END STUDENT CODE
 
     return n, n_normalized
 
+n, n_normalized = compute_normal_vector(P1P2, P2P3, P3P1)
+#print("n:", n)
+#print("n_normalized:", n_normalized)
+
+
 def compute_triangle_area(n:np.ndarray) -> float:
     ### STUDENT CODE
-    # TODO: Implement this function.
+    
+    area = 0.5 * np.linalg.norm(n)
 
-	# NOTE: The following lines can be removed. They prevent the framework
-    #       from crashing.
-
-    area = 0
     ### END STUDENT CODE
 
     return area
 
+area = compute_triangle_area(n)
+#print("area:", area)
+
+
 def compute_angles(P1P2:np.ndarray,P2P3:np.ndarray,P3P1:np.ndarray) -> Tuple[float, float, float]:
     ### STUDENT CODE
-    # TODO: Implement this function.
+    
+    # Length of the triangle sides
+    a = np.linalg.norm(P1P2)
+    b = np.linalg.norm(P2P3)
+    c = np.linalg.norm(P3P1)
 
-	# NOTE: The following lines can be removed. They prevent the framework
-    #       from crashing.
+    # cos of each angle
+    cos_alpha = (b**2 + c**2 - a**2) / (2 * b * c)
+    cos_beta = (a**2 + c**2 - b**2) / (2 * a * c)
+    cos_gamma = (a**2 + b**2 - c**2) / (2 * a * b)
 
-    alpha, beta, gamma = 0., 0., 0.
+    # angles in radian
+    alpha_rad = np.arccos(cos_alpha)
+    beta_rad = np.arccos(cos_beta)
+    gamma_rad = np.arccos(cos_gamma)
+
+    # from radian to degree
+    alpha = np.degrees(alpha_rad)
+    beta = np.degrees(beta_rad)
+    gamma = np.degrees(gamma_rad)
+
     ### END STUDENT CODE
 
     return alpha, beta, gamma
 
+alpha, beta, gamma = compute_angles(P1P2, P2P3, P3P1)
+#print("alpha in degrees:", alpha)
+#print("beta in degrees:", beta)
+#print("gamma in degrees:", gamma)
